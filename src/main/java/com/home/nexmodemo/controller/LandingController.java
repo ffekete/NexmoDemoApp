a77816dffb167f9895d50f16c09bd7eaf7603315
Ferenc Fekete <v-ffekete@hotels.com>
2017-11-14 11:23:57 +0100

    Removed not needed exceptions, moved help message to constant.

--- src/main/java/com/home/nexmodemo/controller/LandingController.java
+++ src/main/java/com/home/nexmodemo/controller/LandingController.java
@@ -1,7 +1,5 @@
 package com.home.nexmodemo.controller;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +11,6 @@ import com.home.nexmodemo.context.TextMessageContext;
 import com.home.nexmodemo.factory.TextMessageContextFactory;
 import com.home.nexmodemo.provider.NexmoSmsContentProvider;
 import com.home.nexmodemo.service.MessageService;
-import com.nexmo.client.NexmoClientException;
 
 /**
  * Handles requests for landing page.
@@ -22,6 +19,7 @@ import com.nexmo.client.NexmoClientException;
 public class LandingController {
 
     private static final String MESSAGE_SENDING_PATH = "/message/to/{number}";
+    private static final String HELP_MESSAGE = "This http method is not supported, please send a POST request instead.";
 
     @Autowired
     private MessageService messageService;
@@ -31,19 +29,16 @@ public class LandingController {
     /**
      * Request handling method for GET requests.
      * @return Result of the message sending.
-     * @throws IOException IO errors.
-     * @throws NexmoClientException Client errors.
      */
     @RequestMapping(path = MESSAGE_SENDING_PATH, method = RequestMethod.POST)
     @ResponseBody
-    public String getResponse(@PathVariable final String number) throws IOException, NexmoClientException {
-        TextMessageContext textMessageContext = TextMessageContextFactory.createFrom(contentProvider.getFrom(), number,
-                contentProvider.getBody());
+    public String getResponse(@PathVariable final String number) {
+        TextMessageContext textMessageContext = TextMessageContextFactory.createFrom(contentProvider.getFrom(), number, contentProvider.getBody());
         return messageService.getMessageSendingResult(textMessageContext);
     }
 
     @RequestMapping(path = MESSAGE_SENDING_PATH)
     public String getDefaultResponse() {
-        return "This http method is not supported, please send a POST request instead.";
+        return HELP_MESSAGE;
     }
 }
