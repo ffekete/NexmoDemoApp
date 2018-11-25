8d43dbeba850581b47aafe3c996bc2e6a07624f4
ffekete84@gmail.com <ffekete84@gmail.com>
2018-11-25 19:13:29 +0000

    Updated hystrix dashboard, stream is now accessible.

---
+++ sms-server/src/main/java/com/home/nexmodemo/configuration/spring/HystrixConfig.java
@@ -0,0 +1,17 @@
+package com.home.nexmodemo.configuration.spring;
+
+import com.codahale.metrics.servlets.AdminServlet;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixConfig {
+
+    @Bean
+    public ServletRegistrationBean hystrixServletRegistrationBean(@Value("${hystrix.stream.url}") String streamUrl) {
+        return new ServletRegistrationBean(new HystrixMetricsStreamServlet(), streamUrl);
+    }
+}
